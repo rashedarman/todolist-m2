ae08af8bdc64febe8535987dc278f71273cd1861
Rashed Arman <rashedarman21@gmail.com>
2022-12-16 23:38:30 +0600

    Remove eslint-disable

--- src/modules/local-storage.js
+++ src/modules/local-storage.js
@@ -1,8 +1,8 @@
 export const TODO_LIST_KEY = 'todo.list';
 
-// eslint-disable-next-line operator-linebreak
-export const todoListObj =
-  JSON.parse(localStorage.getItem(TODO_LIST_KEY)) || [];
+// this to prevent line break by formatter (eslint causes error)
+const _todoListObj = localStorage.getItem(TODO_LIST_KEY);
+export const todoListObj = JSON.parse(_todoListObj) || [];
 
 export const saveTodoObj = () => {
   localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoListObj));
