302e40ae83a5639e6f9d08b0ab8de6325e2b842d
Rashed Arman <rashedarman21@gmail.com>
2022-12-22 23:04:59 +0600

    Test DOM edit, update and clear

--- src/modules/todo-list.test.js
+++ src/modules/todo-list.test.js
@@ -20,7 +20,26 @@ const addTodo = (description, completed = false) => {
   return todo;
 };
 
+const renderTodos = () => {
+  todoListObj.forEach(({ index, description, completed }) => {
+    const checked = completed ? 'checked' : '';
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.setAttribute('data-todo-id', index);
+
+    li.innerHTML = `
+      <input type="checkbox" name="completed" ${checked} />
+      <input type="text" name="description" value="${description}">
+      <ion-icon name="ellipsis-vertical-outline" class="move-btn"></ion-icon>
+      <ion-icon name="trash-outline" class="delete-btn"></ion-icon>
+    `;
+
+    document.body.appendChild(li);
+  });
+};
+
 // Assert
+
 describe('Add & Remove', () => {
   test('Add a todo', () => {
     const todo = new Todo('todo 1');
@@ -63,26 +82,50 @@ describe('Add & Remove', () => {
 describe('Edit, Toggle status and Clear completed', () => {
   test('Edit a todo description', () => {
     const todo = addTodo('todo 1');
+
     const newDescription = 'new todo description';
-
     TodoList.update(todo.index, newDescription);
-
     expect(todo.description).toEqual(newDescription);
 
+    // test dom changes
+    renderTodos();
+
+    const todoEl = document.querySelector(`[data-todo-id="${todo.index}"]`);
+    const todoDescription = todoEl.querySelector('input[name="description"');
+
+    expect(todoDescription.getAttribute('value')).toBe(newDescription);
+
+    // cleanup
     clearTodos();
+    document.body.innerHTML = '';
   });
 
   test('Update completed status', () => {
-    const completedTodo = addTodo('todo 1', true);
-    const pendingTodo = addTodo('todo 2');
+    const todo1 = addTodo('todo 1', true);
+    const todo2 = addTodo('todo 2');
 
-    TodoList.toggleCompleted(completedTodo.index);
-    TodoList.toggleCompleted(pendingTodo.index);
+    // we toggle completed states, so true becomes false
+    TodoList.toggleCompleted(todo1.index);
+    TodoList.toggleCompleted(todo2.index);
 
-    expect(completedTodo.completed).toBe(false);
-    expect(pendingTodo.completed).toBe(true);
+    expect(todo1.completed).toBe(false);
+    expect(todo2.completed).toBe(true);
 
+    // test dom changes
+    renderTodos();
+
+    const todoEl1 = document.querySelector(`[data-todo-id="${todo1.index}"]`);
+    const todoEl2 = document.querySelector(`[data-todo-id="${todo2.index}"]`);
+
+    const checkbox1 = todoEl1.querySelector('input[name="completed"]');
+    const checkbox2 = todoEl2.querySelector('input[name="completed"]');
+
+    expect(checkbox1.hasAttribute('checked')).toBe(false);
+    expect(checkbox2.hasAttribute('checked')).toBe(true);
+
+    // cleanup
     clearTodos();
+    document.body.innerHTML = '';
   });
 
   test('Clear all completed', () => {
@@ -99,7 +142,17 @@ describe('Edit, Toggle status and Clear completed', () => {
       todoListObj.splice(index, 1);
     });
 
-    // We're checking that there's no completed todos
     expect(todoListObj.filter((todo) => todo.completed === 0)).toHaveLength(0);
+
+    // test dom changes
+    renderTodos();
+
+    const checkboxes = document.querySelectorAll('input[name="completed"]');
+    const completedElems = Array.from(checkboxes).filter((elem) => elem.hasAttribute('completed'));
+    expect(completedElems).toHaveLength(0);
+
+    // cleanup
+    clearTodos();
+    document.body.innerHTML = '';
   });
 });
